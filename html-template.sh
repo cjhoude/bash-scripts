fd41eec941ad3e346db7c5473b3ea2d032a942f0
cjhoude <cjhoude@yahoo.com>
2016-07-11 10:54:57 -0400

    Create html-template.sh

---
+++ html-template.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script produces a simple HTML file if you pipe it to new file.
+# Note the use of quotes to cover multiple lines.
+
+echo "<html>
+ <head>
+   <title>
+   The title of your page
+   </title>
+ </head>
+
+ <body>
+   Your page content goes here.
+ </body>
+ </html>"
