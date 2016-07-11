20c7e7dbe09a492a1984e2d3288c83bd02dafc9f
cjhoude <cjhoude@yahoo.com>
2016-07-11 10:50:03 -0400

    Create add-txt.sh

---
+++ add-txt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Simple test of how to add a file extension to a file name.
+# Would be handy if you had to add a file extension to a lot of files. 
+# add-txt.sh <file name> 
+mv $1 $1.txt
