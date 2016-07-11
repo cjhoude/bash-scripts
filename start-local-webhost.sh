48a955b5623bd90f3265c72161dcd3d01c6eabed
cjhoude <cjhoude@yahoo.com>
2016-07-11 10:45:04 -0400

    Create start-local-webhost.sh

---
+++ start-local-webhost.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Starts local webhost http-server installed with npm.
+# This is mostly just to save me the trouble of typing it.
+# Hit CTRL-C to stop the server from running.
+nodejs /home/kortni/node_modules/http-server/bin/http-server
+
+# You can visit your server at http://localhost:8080 
